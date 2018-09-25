b9a3aa2f63f9f5c36e3ae1998707385a22a1214f
KostyaTurchenko <34889744+KostyaTurchenko@users.noreply.github.com>
2018-09-25 16:51:00 +0300

    Sequences

---
+++ Sequences/BL/SequenceCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BL
+{
+    public partial class SequenceCalculator
+    {
+        public double X { get; set; }
+        public int N { get; set; }
+        public double E { get; set; }
+
+        public int NLimited { get; private set; }
+
+        public double Fault { get; private set; }
+
+        public SequenceCalculator(double X, int N)
+        {
+            this.X = X;
+            this.N = N;
+        }
+
+        private double Factorial(int numb)
+        {
+            double result = 1;
+            for (int i = numb; i > 1; i--)
+                result *= i;
+            return result;
+        }
+
+        public double GetPartialSum ()
+        {
+            double sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                sum += Math.Pow(-1, i) * Math.Pow(X, i) / Factorial(i);
+            }
+
+            Fault = Math.Abs(Math.Pow(Math.E, -X) - sum);
+
+            return sum;
+        }
+
+        public double GetSumLimitedE (int multiplier)
+        {
+            double EComparable = E * multiplier;
+            double sum = 0;
+            double term = 0;
+            int counter = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                term = Math.Pow(-1, i) * Math.Pow(X, i) / Factorial(i);
+                if (Math.Abs(term) > EComparable)
+                {
+                    sum += term;
+                    counter++;
+                }
+                else
+                    continue;
+            }
+            NLimited = counter;
+
+            return sum;
+
+        }
+
+        public double CompareFaultAndLast() => Fault - Math.Abs(Math.Pow(-1, N) * Math.Pow(X, N) / Factorial(N));
+
+
+
+    }
+}
---
+++ Sequences/task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BL;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("task 1");
+
+            Console.WriteLine("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            SequenceCalculator MyCalc = new SequenceCalculator(x, n);
+
+
+            Console.WriteLine("Частичная сумма = {0}", MyCalc.GetPartialSum());
+            Console.WriteLine("Погрешность = {0}", MyCalc.Fault);
+            Console.WriteLine("Разница между погрешностью и N-го слагаемого = {0}", MyCalc.CompareFaultAndLast());
+            Console.WriteLine("Значение e^(-{0}) = {1}",x, Math.Pow(Math.E, -x));
+
+
+            Console.WriteLine(new String('-', 65));
+
+
+            Console.WriteLine("task 2");
+
+            Console.WriteLine("Введите E: ");
+            MyCalc.E = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите x: ");
+            MyCalc.X = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Cумма слагаемых больше E = {0}", MyCalc.GetSumLimitedE(1));
+            Console.WriteLine("Количество таких слагаемых = {0}", MyCalc.NLimited);
+            Console.WriteLine("Сумма слагаемых больше E*10 = {0}", MyCalc.GetSumLimitedE(10));
+
+
+
+        }
+    }
+}
